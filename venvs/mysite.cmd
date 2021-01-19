fd1c218a1ad072b017e617eb2859a92c77beb13d
pyk707 <pyk707@hanmail.net>
2021-01-19 22:08:40 +0900

    210119 update

---
+++ venvs/mysite.cmd
@@ -0,0 +1,3 @@
+@echo off
+cd C:/Users/pyk70/OneDrive/@coding/python/jump_to_jango/jump_to_jango/projects/mysite
+C:/Users/pyk70/OneDrive/@coding/python/jump_to_jango/jump_to_jango/venvs/mysite/Scripts/activate
